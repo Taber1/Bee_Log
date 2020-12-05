22392efbdfdf1dbc75375f6e12191d0255c9e907
Taber Bin Zameer <tbzameer@gmail.com>
2020-12-06 03:51:51 +0500

    provider starts

--- lib/Models/posts.dart
+++ lib/Models/posts.dart
@@ -9,4 +9,25 @@ class Posts {
 
   Posts(this.date, this.description, this.image, this.time, this.title, this.id,
       this.fav);
+
+  Posts.fromMap(Map snapshot, String id)
+      : id = id ?? '',
+        date = snapshot['date'] ?? '',
+        description = snapshot['description'] ?? '',
+        image = snapshot['image'] ?? '',
+        time = snapshot['time'] ?? '',
+        title = snapshot['title'] ?? '',
+        fav = snapshot['fav'] ?? '';
+
+  toJson() {
+    return {
+      'id': id,
+      'date': date,
+      'description': description,
+      'image': image,
+      'time': time,
+      'title': title,
+      'fav': fav,
+    };
+  }
 }
